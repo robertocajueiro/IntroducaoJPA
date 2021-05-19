d1e6ec95c5101f9ec001e2e64fb9b468768e2f5f
Roberto Cajueiro <beto.cajueiro@gmail.com>
2021-05-19 11:32:36 -0300

    Configurando Flyway

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,10 +1,10 @@
 package com.algaworks.algalog.domain.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
+@Data
 public class Cliente {
 	
 	private Long id;
